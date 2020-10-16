5bf5ad60bfc17ae07457b854f9d84dcd1146a490
EdgarSalasG <esalas@guaodev.com>
2020-10-16 14:32:05 -0500

    added components and their routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { XyzComponent } from './xyz/xyz.component';
+import { ProductComponent } from './product/product.component';
+import { TableProductComponent } from './product/table-product/table-product.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'xyz', component: XyzComponent },
+  {
+    path: 'product',
+    children: [
+      { path: '', component: ProductComponent },
+      { path: 'table', component: TableProductComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
+import { TableProductComponent } from './product/table-product/table-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    ProductComponent,
+    HomeComponent,
+    TableProductComponent
   ],
   imports: [
     BrowserModule,
